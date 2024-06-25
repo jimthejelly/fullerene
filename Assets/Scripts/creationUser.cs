a55baf4bdf24a1a1caad42ee5133d0d32165dad1
jimthejelly <jimincodes@gmail.com>
2024-06-25 14:32:39 -0400

    small update 2

---
+++ Assets/Scripts/creationUser.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creationUser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elementTurn();
+    }
+
+    //turn moleculeBody
+    void elementTurn()
+    {
+
+    }
+
+    //script to add element to moleculeBody
+    void elementAdd()
+    {
+
+    }
+
+    void elementDelete()
+    {
+
+    }
+}
