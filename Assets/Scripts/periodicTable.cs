dd3dec76938e7c231f8bed23e14eabd73a826244
jimthejelly <jimincodes@gmail.com>
2024-07-12 15:48:54 -0400

    Added toggle for lanthanides + acinides
    button to toggle to show lanthanides and actinides

---
+++ Assets/Scripts/periodicTable.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class periodicTable : MonoBehaviour
+{
+    public GameObject lanthanide;
+    public GameObject actinide;
+
+    public bool showedLanthanide;
+    public bool showedActinide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lanthanide.SetActive(false);
+        showedLanthanide = false;
+        actinide.SetActive(false);
+        showedActinide = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void lanthanideToggle()
+    {
+        if (showedLanthanide)
+        {
+            lanthanide.SetActive(false);
+            showedLanthanide = false;
+        }
+        else
+        {
+            lanthanide.SetActive(true);
+            showedLanthanide = true;
+        }
+    }
+
+    public void actinideToggle()
+    {
+        if (showedActinide)
+        {
+            actinide.SetActive(false);
+            showedActinide = false;
+        }
+        else
+        {
+            actinide.SetActive(true);
+            showedActinide = true;
+        }
+    }
+}
---
+++ Assets/table_btn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class table_btn : MonoBehaviour
+{
+    public GameObject pause_menu;
+    public GameObject table_menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
