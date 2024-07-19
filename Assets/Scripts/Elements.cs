7463428afd6ea54d651fc6cd3f284c41de594589
cbello14 <cyrusbello@gmail.com>
2024-07-19 14:58:34 -0400

    Spawning new elements
    Elements.cs updated with new code, and bonds are separate objects in between the parent and child elements

--- Assets/Scripts/Elements.cs
+++ Assets/Scripts/Elements.cs
@@ -21,18 +21,26 @@ public class Elements : MonoBehaviour
     }
 
     void OnMouseDown() {
-        if(Input.GetKeyDown("left shift")) {
+        //if(Input.GetKeyDown("left shift")) {
             SpawnElement();
-        }
+        //}
+    }
+
+    // returns (a.x*b.x, a.y*b.y, a.z*b.z)
+    Vector3 indivMult(Vector3 a, Vector3 b) {
+        return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
     }
     // Spawns the element prefab contained in the global variable element
     void SpawnElement() {
-        GameObject tempObj = AssetDatabase.LoadAssetAtPath("Assets/Elements/" /*+ selectElement.element */+ ".prefab", typeof(GameObject)) as GameObject;
-        GameObject clone = Instantiate(tempObj, Vector3.zero, Quaternion.identity, transform);
-        float radius;
+        GameObject cyl = AssetDatabase.LoadAssetAtPath("Assets/Resources/SingleBond.prefab", typeof(GameObject)) as GameObject;
+        GameObject cylClone = Instantiate(cyl, Vector3.zero, Quaternion.identity, transform);
+        GameObject obj = AssetDatabase.LoadAssetAtPath("Assets/Elements/" + selectElement.element + ".prefab", typeof(GameObject)) as GameObject;
+        GameObject clone = Instantiate(obj, Vector3.zero, Quaternion.identity);
+        clone.transform.SetParent(cylClone.transform, true);
+        float radius = 3f;
         int bondCount = 0;
         foreach(Transform child in transform) {
-            if(child.tag.Equals("Element")) {
+            if(child.tag.Equals("Bond")) {
                 bondCount++;
             }
         }
@@ -41,37 +49,26 @@ public class Elements : MonoBehaviour
             bondCount++;
             start = 1;
         }
-        Debug.Log(bondCount);
+        for(int i = start; i < bondCount; i++) {
+            transform.GetChild(i).localPosition = Vector3.up * radius / 2;
+            transform.GetChild(i).localEulerAngles = new Vector3(180, 0, 0);
+        }
+        transform.GetChild(bondCount - 1).GetChild(0).localEulerAngles = transform.GetChild(bondCount - 1).localEulerAngles + new Vector3(180, 0, 0);
+        clone.transform.localPosition = indivMult(cylClone.transform.up * radius / 2, clone.transform.localScale);
         if(bondCount == 1) {
-            radius = 3f;
-            transform.GetChild(0).localPosition = Vector3.up * radius;
+            transform.GetChild(0).localPosition = Vector3.up * radius / 2;
             transform.GetChild(0).localEulerAngles = new Vector3(180, 0, 0);
         }
         else if(bondCount == 2) {
-            for(int i = start; i < 2; i++) {
-                radius = 3f;
-                transform.GetChild(i).localPosition = Vector3.up * radius;
-                transform.GetChild(i).localEulerAngles = new Vector3(180, 0, 0);
-            }
             transform.GetChild(1).RotateAround(transform.position, transform.forward, 180);
         }
         else if(bondCount == 3) {
-           for(int i = start; i < 3; i++) {
-                radius = 3f;
-                transform.GetChild(i).localPosition = Vector3.up * radius;
-                transform.GetChild(i).localEulerAngles = new Vector3(180, 0, 0);
-            }
             transform.GetChild(1).RotateAround(transform.position, transform.forward, 120);
             transform.GetChild(2).RotateAround(transform.position, transform.forward, 240);
         }
         else if(bondCount == 4) {
-           for(int i = start; i < 4; i++) {
-                radius = 3f;
-                transform.GetChild(i).localPosition = Vector3.up * radius;
-                transform.GetChild(i).localEulerAngles = new Vector3(180, 0, 0);
-                if(i > 0) {
-                    transform.GetChild(i).RotateAround(transform.position, transform.right, 109);
-                }
+            for(int i = 1; i < 4; i++) {
+                transform.GetChild(i).RotateAround(transform.position, transform.right, 109);
             }
             transform.GetChild(2).RotateAround(transform.position, transform.up, 109);
             transform.GetChild(3).RotateAround(transform.position, transform.up, -109);
